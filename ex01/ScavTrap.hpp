718081a71ddf4a11eda154f0662bd0765bdc9f05
Nvnrs <nicolasvenerosy@gmail.com>
2025-06-23 11:16:58 +0200

    remove virtual child ex01

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -8,13 +8,13 @@ class ScavTrap : public ClapTrap {
 
 	private:
 	    ScavTrap();
-	
+
 	public:
 	    ScavTrap(std::string name);
 	    ScavTrap(const ScavTrap &object);
 	    ScavTrap &operator=(ScavTrap const &object);
-	    virtual ~ScavTrap();
-	
+	    ~ScavTrap();
+
 	    void attack(const std::string &target);
 	    void guardGate();
 };
