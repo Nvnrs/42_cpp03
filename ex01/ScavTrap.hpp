52e691293aa5d90eac789483e519c93d9ae57f1f
Nvnrs <nicolasvenerosy@gmail.com>
2025-06-23 11:10:53 +0200

    clean and end ex01

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -2,63 +2,21 @@
 #define SCAVTRAP
 
 #include "ClapTrap.hpp"
+#include <iostream>
 
-class ScavTrap: public ClapTrap {
+class ScavTrap : public ClapTrap {
 
-	private :
-		ScavTrap()
-		{
-			std::cout << "SCAVTRAP Default constructor called\n";
-		}
-
-	public :
-
-		ScavTrap(std::string name) : ClapTrap(name)
-		{
-			std::cout << "SCAVTRAP Constructor params called\n";
-		};
-
-		ScavTrap(const ScavTrap &object) : ClapTrap(object)
-		{
-			std::cout << "SCAVTRAP Constructor copy called \n";
-			*this = object;
-		};
-
-		ScavTrap &operator=(ScavTrap const &object)
-		{
-			ClapTrap::operator=(object);
-			return *this;
-		};
-
-		void attack(const std::string &target)
-		{
-			if (this->checkIsDead() || !this->checkHaveEnergy())
-				return;
-			std::cout << "SCAVTRAP "
-				<< this->name
-				<< " attacks "
-				<< target
-				<< " causing, "
-				<< this->attackDamage
-				<< " points of damage !"
-				<< std::endl;
-			this->energyPoints--;
-		}
-
-		~ScavTrap()
-		{
-			std::cout << "SCAVTRAP Destructor params called\n";
-		}
-
-		void guardGate()
-		{
-			if (checkIsDead())
-				return;
-			std::cout << "ScavTrap "
-				<< this->name
-				<< " is now in Gate keeper mode\n";
-
-		}
+	private:
+	    ScavTrap();
+	
+	public:
+	    ScavTrap(std::string name);
+	    ScavTrap(const ScavTrap &object);
+	    ScavTrap &operator=(ScavTrap const &object);
+	    virtual ~ScavTrap();
+	
+	    void attack(const std::string &target);
+	    void guardGate();
 };
 
 #endif
